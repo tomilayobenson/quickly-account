86fe4100ae922e5618a7d4082947414a2b401d02
TomilayoAfolabi <tomilayoafolabi@gmail.com>
2023-05-03 15:18:45 -0600

    setup basic file structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Login from './views/Login';
+import Signup from './views/Signup';
+import Profile from './views/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='signup' element={<Signup />} />
+        <Route path='profile' element={<Profile />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
